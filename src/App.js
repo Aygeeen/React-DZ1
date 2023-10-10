b2471247444b34ff315a90d74e9f5d90b937ab6d
Aygeeen <wreckingkraken@gmail.com>
2023-10-10 23:53:43 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles/input.css"
+import "./styles/button.css"
+function App() {
+  return (
+    <div className="search">
+      <input type="text" placeholder="search" className="input"/>
+      <button className="button">Advanced Search</button>
+    </div>
+  );
+}
+
+export default App;
